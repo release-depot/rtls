2c4822e6391a9bb3b79e580b61bc59ffbfe2edde
Julie Pichon <jpichon@redhat.com>
2020-07-23 15:26:45 +0100

    Updated cookiecutter structure

--- docs/installation.rst
+++ docs/installation.rst
@@ -14,7 +14,7 @@ To install Real Time Locator System, run this command in your terminal:
 
     $ pip install rtls
 
-This is the preferred method to install Real Time Locator System, as it will always install the most recent stable release. 
+This is the preferred method to install Real Time Locator System, as it will always install the most recent stable release.
 
 If you don't have `pip`_ installed, this `Python installation guide`_ can guide
 you through the process.
@@ -38,7 +38,7 @@ Or download the `tarball`_:
 
 .. code-block:: console
 
-    $ curl  -OL https://github.com/release-depot/rtls/tarball/master
+    $ curl -OJL https://github.com/release-depot/rtls/tarball/master
 
 Once you have a copy of the source, you can install it with:
 
--- tests/test_rtls.py
+++ tests/test_rtls.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """Tests for `rtls` package."""
 
+import pytest
 
-import unittest
 
 from rtls import rtls
 
 
-class TestRtls(unittest.TestCase):
-    """Tests for `rtls` package."""
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
 
-    def setUp(self):
-        """Set up test fixtures, if any."""
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    # import requests
+    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
 
-    def test_000_something(self):
-        """Test something."""
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument."""
+    # from bs4 import BeautifulSoup
+    # assert 'GitHub' in BeautifulSoup(response.content).title.string
